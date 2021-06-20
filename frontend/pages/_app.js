f2790bf914a7b7922274d018827401fd645db4fb
Sebastian <sebastian.sloniec@gmail.com>
2021-06-20 17:02:46 +0200

    Setup apollo, ssr styles fix

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,34 @@
-import NPprogress from 'nprogress';
-import Router from 'next/router';
+import NPprogress from "nprogress";
+import Router from "next/router";
+import { ApolloProvider } from "@apollo/client";
 
+import apolloWithData from "../lib/apolloWithData";
 import Page from "../components/Page";
 
-import '../components/styles/nprogress.css';
-Router.events.on('routeChangeStart', () => NPprogress.start());
-Router.events.on('routeChangeComplete', () => NPprogress.done());
-Router.events.on('routeChangeError', () => NPprogress.done());
+import "../components/styles/nprogress.css";
+Router.events.on("routeChangeStart", () => NPprogress.start());
+Router.events.on("routeChangeComplete", () => NPprogress.done());
+Router.events.on("routeChangeError", () => NPprogress.done());
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default apolloWithData(App);
