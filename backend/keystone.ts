68d9423f9b868d05f5d48780a23bbb27aa6662fc
Sebastian <sebastian.sloniec@gmail.com>
2021-06-20 11:54:07 +0200

    Basic Keystone configuration

---
+++ backend/keystone.ts
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import { createAuth } from "@keystone-next/auth";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+
+import { User } from "./schemas/User";
+
+const databaseURL =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-basicshop";
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 7,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseURL,
+    },
+    lists: createSchema({
+      User,
+    }),
+    ui: {
+      isAccessAllowed: ({ session }) => !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      User: `id name email`,
+    }),
+  })
+);
