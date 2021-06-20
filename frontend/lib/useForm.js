ee8399b7ad0072bcdab71c58962f3c553d21a83b
Sebastian <sebastian.sloniec@gmail.com>
2021-06-20 19:36:57 +0200

    create useForm hook

---
+++ frontend/lib/useForm.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+export default function useForm(initialState = {}) {
+  const [inputs, setInputs] = useState(initialState);
+
+  function handleChange(e) {
+    let { value, name, type } = e.target;
+
+    if (type === "number" || name === "price") {
+      console.log(value);
+      value = parseInt(value);
+    }
+
+    if (type === "file") {
+      [value] = e.target.files;
+    }
+
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  }
+
+  function resetForm() {
+    setInputs(initialState);
+  }
+
+  function clearForm() {
+    const blankState = Object.fromEntries(
+      Object.entries(inputs).map(([key, value]) => [key, ""])
+    );
+    setInputs(blankState);
+  }
+
+  return {
+    inputs,
+    handleChange,
+    resetForm,
+    clearForm
+  };
+}
